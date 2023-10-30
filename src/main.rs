17f30163b8298ddfb569c0cccf380151ac14e6d0
Дмитрий Филин <dafilin@sberbank.ru>
2023-10-30 13:26:02 +0300

    pass 3st stage

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
 use serde_json;
 use std::env;
 use serde_json::Number;
+use serde_bencode;
 
-// Available if you need it!
-// use serde_bencode
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        let number: i64 = encoded_value[1..encoded_value.len() - 1].parse().unwrap();
-        return serde_json::Value::Number(Number::from(number));
-    }
+    serde_bencode::from_str(encoded_value).unwrap()
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
